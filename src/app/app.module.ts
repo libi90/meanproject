d7d39d7c4803e5d68942d4f8c9dc0300905f4ac5
libi <libibabu90@gmail.com>
2021-02-09 12:16:52 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{FormsModule}from '@angular/forms'
+import{ReactiveFormsModule}from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +31,7 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner"
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+   ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 
@@ -17,25 +17,19 @@ export class PostCreateComponent implements OnInit {
   enteredContent=""
   post:Post;
   isLoading=false;
+  form:FormGroup;
+  imagePreview:string;
   private mode='create';
   private postId:string;
-  
-  
-  onSavePost(form:NgForm){
-    if(form.invalid){
-      return;
-    }
-    this.isLoading=true;
-    if(this.mode==="create"){
-      this.postsService.addPost(form.value.title,form.value.content)
-    }else{
-      this.postsService.updatePost(this.postId,form.value.title,form.value.content)
-    }
-     form.resetForm();
-  }
-
+     
 ngOnInit(): void {
-    this.route.paramMap.subscribe((paramMap:ParamMap)=>{
+  this.form=new FormGroup({
+    'title':new FormControl(null,{validators:[Validators.required,Validators.minLength(3)]}),
+    'content': new FormControl(null,{validators:[Validators.required]}),
+    'image': new FormControl(null,{validators:[Validators.required]})
+  });
+  
+      this.route.paramMap.subscribe((paramMap:ParamMap)=>{
       if(paramMap.has('postId')){
         this.mode='edit';
         this.postId=paramMap.get('postId');
@@ -43,7 +37,7 @@ ngOnInit(): void {
         this.postsService.getPost(this.postId).subscribe(postData=>{
           this.isLoading=false;
           this.post={id:postData._id,title:postData.title,content:postData.content}
-        
+        this.form.setValue({'title':this.post.title,'content':this.post.title});
 })
   }else{
         this.mode='create';
@@ -51,5 +45,31 @@ ngOnInit(): void {
       }
     })
   }
-
+  onImagePicked(event: Event){
+    const file=(event.target as HTMLInputElement) .files[0];
+ this.form.patchValue({image:file});
+ this.form.get('image').updateValueAndValidity();
+  const reader=new FileReader();
+ reader.onload=()=>{
+  this.imagePreview=reader.result as string;
 }
+reader.readAsDataURL(file)
+}
+  onSavePost(){
+    if(this.form.invalid){
+      return;
+    }
+    this.isLoading=true;
+    if(this.mode==="create"){
+      this.postsService.addPost(this.form.value.title,this.form.value.content)
+    }else{
+      this.postsService.updatePost(this.postId,this.form.value.title,this.form.value.content)
+    }
+   this.form.reset();
+  }
+
+  
+  }
+  
+
+
